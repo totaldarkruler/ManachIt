576a5781084210de1c0fcbd7ab75ab09a9325b1d
jacer <jacer@DESKTOP-J44MVLI>
2016-08-01 21:22:34 -0700

    Add project files.

---
+++ ManachIt/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ManachIt.MvcApplication" Language="C#" %>
---
+++ ManachIt/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ManachIt.Startup))]
+namespace ManachIt
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
