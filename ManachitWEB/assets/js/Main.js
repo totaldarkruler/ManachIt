b9ea46e536a69364176b4878d9703d4ef328226c
Daniel Alexandro Hernandez <dannix.88@gmail.com>
2016-08-28 20:06:12 -0700

    Cambios

--- ManachitWEB/assets/js/Main.js
+++ ManachitWEB/assets/js/Main.js
@@ -1,10 +1,7 @@
-﻿// create the module and name it scotchApp
-    var scotchApp = angular.module('scotchApp', []);
+﻿// create the module
+var manageitApp = angular.module('manageitApp', []);
 
-    // create the controller and inject Angular's $scope
-    scotchApp.controller('mainController', function($scope) {
-	
+// create the controller and inject Angular's $scope
+manageitApp.controller('mainController', function ($scope) {
 
-        // create a message to display in our view
-        $scope.message = 'Everyone come and see how good I look!';
-    });
+});
